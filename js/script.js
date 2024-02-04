96823f5f47e67fed537ccf69cbee6e7e859d1cdd
Magdalena Makuch <makuchmagda02@wp.pl>
2024-02-04 23:14:25 +0100

    added js function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-console.log("Cześć! Witam wszystkich deweloperów");
+{
+    const welcome = () => {
+        console.log("Cześć! Witam wszystkich deweloperów");
+    };
 
+    const toggleBackgraund = () => {
+        const header = document.querySelector(".js-header");
+        const text = document.querySelector(".js-text");
 
-let header = document.querySelector(".js-header");
-let button = document.querySelector(".js-button");
-let text = document.querySelector(".js-text");
+        header.classList.toggle("dark");
+        text.innerText = header.classList.contains("dark") ? "Wyłącz" : "Włącz";
+    };
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackgraund);
 
+        welcome();
+    };
 
-button.addEventListener("click", () => {
-    header.classList.toggle("dark");
-    text.innerText = header.classList.contains("dark") ? "Wyłącz" : "Włącz";
-});
+    init();
+}
+
